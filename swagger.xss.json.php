cb4a4c78d8256dc6c8d8d1d079bf453bba9abdf9
Lokii808 <64386219+Lokii808@users.noreply.github.com>
2023-12-03 10:31:34 -0300

    Add files via upload

---
+++ swagger.xss.json.php
@@ -0,0 +1,9 @@
+{
+    "url": "https://xxx.dbggl.pw/swagger.xss.yaml.php",
+    "urls": [
+        {
+            "url": "https://xxx.dbggl.pw/swagger.xss.yaml.php",
+            "name": "Foo"
+        }
+    ]
+}
